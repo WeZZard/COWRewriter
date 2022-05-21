eb2962b2ea0cb70474602153027334b92813c448
WeZZard <me@wezzard.com>
2022-05-21 14:12:01 +0800

    Initial Commit

---
+++ COWRewriter/COWRewriterApp.swift
@@ -0,0 +1,17 @@
+//
+//  COWRewriterApp.swift
+//  COWRewriter
+//
+//  Created by WeZZard on 5/21/22.
+//
+
+import SwiftUI
+
+@main
+struct COWRewriterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ COWRewriter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  COWRewriter
+//
+//  Created by WeZZard on 5/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
